430d41ef20a40130414c2755d3aead8203279f88
Md Harunur Rashid <88606531+Harun602@users.noreply.github.com>
2022-02-23 06:20:56 -0800

    Added this is file Hello World
    Hello World

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+// var firstName = "Rahim";
+// var age = 20;
+// var isMarried = false;
+
+
+// var firstNumber = 100;
+// var secondNumber = 200;
+// var result = firstNumber + secondNumber;
+
+var one = "hello";
+var two = "World";
+var final = one + " " + two; //concatanation
+
+var amountInDollar = 40;
+var currentDollarRate = 85.45;
+var amountInBdt = amountInDollar * currentDollarRate;
+
+function sayHello(){
+    var usersName = document.getElementById("name").value;
+    alert("Hello " + usersName);
+}
+
+
+//alert(final);
